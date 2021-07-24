a3aae806dcbef92bea971e9f9cf07e1d3d534bff
ZijieXiong <zx811@nyu.edu>
2021-07-23 22:19:08 -0400

    Add conversation class that stores multiple dialogues and implement it in DialogueTrigger

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Conversation conversation;
 
     public void TriggerDialogue ()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(conversation.dialogues[0]);
     }
 
 }
