144aeff142b344f7c6698b99059801248cd03470
ZijieXiong <zx811@nyu.edu>
2021-07-30 22:21:29 -0400

    Start Prologue when starting a new game and correctly enter PlayScene when game starts

--- Assets/Scripts/StartMenuManager.cs
+++ Assets/Scripts/StartMenuManager.cs
@@ -7,10 +7,11 @@ using static DatabaseManager;
 public class StartMenuManager : MonoBehaviour
 {
     public GameObject prologue;
+    public ConversationManager cm;
     public void NewGame()
     {
         DatabaseManager.ResetDatabase();
-        GameStart();
+        cm.StartConversation(0);
     }
 
     public void Continue()
@@ -19,7 +20,9 @@ public class StartMenuManager : MonoBehaviour
     }
 
     private void GameStart()
-    {}
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +35,9 @@ public class StartMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(cm.isEnd)
+        {
+            GameStart();
+        }
     }
 }
